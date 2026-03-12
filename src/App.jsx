925f0745957c21ba780d8f964c0679ad7971ebab
erxanz <172484226+erxanz@users.noreply.github.com>
2026-03-12 13:47:54 +0700

    State useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,45 @@
-// fungsi usedState digunakan untuk membuat state di dalam komponen fungsional.
-// State adalah data yang dapat berubah-ubah dan mempengaruhi tampilan komponen.
-// Dengan menggunakan useState, kita dapat menyimpan data yang dapat berubah-ubah
-// dan memperbarui tampilan komponen ketika data tersebut berubah.
 import { useState } from "react";
 
 import "./App.css";
 
 function App() {
+  // 1. Memebuat state dasar
   const [message, setMessage] = useState("Halo, React!");
+
+  // 2. State dengan angka
   const [count, setCount] = useState(0);
+
+  // 3. State menyimpan array/objek
+  const [todos, setTodos] = useState([
+    "Belajar React",
+    "Belajar JSX"
+  ]);
+
+  const addTodo = () => {
+    setTodos([... todos, "Belajar State", "Belajar Props"]);
+  }
 
   return (
     <>
       <div>
-        <h3>{message}</h3>
+        {/* 1. Membuat state dasar */}
+        <h4>{message}</h4>
         <button onClick={() => setMessage("Pesan diubah!")}>Ubah pesan</button>
 
-        <h5>Count: {count}</h5>
+        {/* 2. State dengan angka */}
+        <h4>Count: {count}</h4>
         <button onClick={() => setCount(count + 1)}>Tambah</button>
         <button onClick={() => setCount(count - 1)}>Kurang</button>
       </div>
+
+      {/* 3. State menyimpan array/objek */}
+      <h4>Daftar Todo:</h4>
+      <ul>
+        { todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+        <button onClick={addTodo}>Tambah Todo</button>
+      </ul>
     </>
   );
 }
